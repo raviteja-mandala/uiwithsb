737cd4a5528b6ea20e12cb12a208c757d5d5ac03
raviteja-mandala <raviteja.mandala@zemosolabs.com>
2021-08-10 20:35:16 +0530

    835pm

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    presets: ["@babel/preset-react"]
-
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    env: {
+        test: {
+            plugins: ["@babel/plugin-transform-runtime"]
+        }
+    }
 };
